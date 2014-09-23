063ab43fc86db2c6e64e3a459fc695a6e241f0e3
mac <mac@mac-no-MacBook-Air.local>
2014-09-23 11:07:32 +0900

    wear notification

---
+++ wear/src/main/java/jp/yuruga/catchme/CatchMeWearableListenerService.java
@@ -0,0 +1,97 @@
+package jp.yuruga.catchme;
+
+import android.util.Log;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
+
+/**
+ * Created by mac on 2014/09/23.
+ */
+public class CatchMeWearableListenerService extends WearableListenerService {
+    private static final String TAG = "CatchMeWearableListenerService";
+    public static final String POSITION_PATH = "/position";
+    public static final String POSITION_KEY = "position";
+    public static final String CATCH_ACTION_PATH = "/catch";
+    public CatchMeWearableListenerService() {
+    }
+
+    GoogleApiClient mGoogleApiClient;
+    @Override
+    public void onCreate () {
+        Log.d(TAG,"onCreate");
+        super.onCreate();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(Bundle connectionHint) {
+                        Log.d(TAG, "onConnected: " + connectionHint);
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int cause) {
+                        Log.d(TAG, "onConnectionSuspended: " + cause);
+                    }
+                })
+                .addApi(Wearable.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        Log.d(TAG, "onDataChanged");
+        for (DataEvent event : dataEvents) {
+            if (event.getType() == DataEvent.TYPE_DELETED) {
+                Log.d(TAG, "DataItem deleted: " + event.getDataItem().getUri());
+            } else if (event.getType() == DataEvent.TYPE_CHANGED) {
+                Log.d(TAG, "DataItem changed: " + event.getDataItem().getUri());
+                if (event.getDataItem().getUri().getPath().equals(POSITION_PATH)) {
+                    DataMap map = DataMapItem.fromDataItem(event.getDataItem()).getDataMap();
+                    int distance = map.getInt(POSITION_KEY);
+                    Log.d(TAG, "distance:" + distance);
+                    NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+                    Notification notification = new Notification.Builder(this)
+                            .setContentTitle("test")
+                            .setContentText("distance:" + distance)
+                            .setSmallIcon(R.drawable.ic_launcher)
+                            .build();
+                    notification.contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+                    nm.notify(0, notification);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onPeerConnected(Node peer) {
+        LOGD(TAG, "onPeerConnected: " + peer);
+    }
+    @Override
+    public void onPeerDisconnected(Node peer) {
+        LOGD(TAG, "onPeerDisconnected: " + peer);
+    }
+
+    public static void LOGD(final String tag, String message) {
+        if (Log.isLoggable(tag, Log.DEBUG)) {
+            Log.d(tag, message);
+        }
+    }
+}
